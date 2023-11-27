0e602774e34bd4aa986e296f8be17962ae0c7f4d
Daniel Asena <asenadaniel@gmail.com>
2023-11-28 00:45:45 +0100

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,7 +5,7 @@ function Products({ products }) {
   return (
     <div className=' py-10 dark:bg-black  dark:text-gray-100 '>
       <div className='flex flex-col items-center gap-4'>
-        <h1 className='md:text-1xl dark:text-gray-100  dark:bg-white dark:text-black bg-black text-white py-2 w-80 text-center '>Shopping day in, day out.</h1>
+        <h1 className='md:text-1xl dark:bg-white dark:text-black bg-black text-white py-2 w-80 text-center '>Shopping day in, day out.</h1>
         <span className='w-20 h-[3px] bg-black'></span>
         <p className='max-w-[700px] dark:text-gray-100 text-gray-600 text-center text-xs'>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores quasi numquam voluptate delectus? Odit ratione, dignissimos et aliquid minima necessitatibus.
