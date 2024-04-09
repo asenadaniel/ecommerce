dfe598a889fb262f01031b7e8ef20cd451700cf5
Daniel Asena <asenadaniel@gmail.com>
2024-04-09 11:56:48 +0100

    updated the api

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const productData = async () => {
-  const products = await axios.get('https://fakestoreapiserver.reactbd.com/products'
+  const products = await axios.get('https://fakestoreapi.com/products'
   );
 
   if (products.ok) {
